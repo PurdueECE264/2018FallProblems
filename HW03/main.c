a6fc0acebfc09ed6adfb3a26c99dbf8e6fc880f6
Somosmita Mitra <42362390+Somosmita@users.noreply.github.com>
2018-08-22 01:48:22 +0530

    Update main.c

--- HW03/main.c
+++ HW03/main.c
@@ -12,7 +12,7 @@ int main(int argc, char * * argv)
     }
   int valn = (int) strtol(argv[1], NULL, 10);
   int valk = (int) strtol(argv[2], NULL, 10);
-  if ((valn < 1) || (valk < 1))
+  if ((valn <= 1) || (valk <= 1))
     {
       fprintf(stderr, "need two numbers greater than 1\n");
       return EXIT_FAILURE;
