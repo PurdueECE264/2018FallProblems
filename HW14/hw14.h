f6546c7a8858b787c2227f1fbef193a4929fb9e2
Abhinav Goel <abhinavgoel95@gmail.com>
2018-10-05 14:12:12 -0400

    Update hw14.h

--- HW14/hw14.h
+++ HW14/hw14.h
@@ -10,11 +10,11 @@ typedef struct ListNode
 } Node;
 
 
-Node * CreateNode(int val)
-void LinkedListCreate(Node * * source, int len, int* arr)
+Node * CreateNode(int val);
+void LinkedListCreate(Node * * source, int len, int* arr);
 void LinkedListPrint(Node ** head);
-void MergeSort(Node** source) 
-Node* Merge(Node* upper, Node* lower) 
+void MergeSort(Node** source); 
+Node* Merge(Node* upper, Node* lower); 
 void SpiltList(Node* source, Node** head1, Node** head2);
 
 #endif
