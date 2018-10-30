5bcb9c77ed96f1e680ecfccd0d85817a725d22b6
Abhinav Goel <abhinavgoel95@gmail.com>
2018-10-29 20:53:10 -0400

    Update hw14.h

--- HW14/hw14.h
+++ HW14/hw14.h
@@ -15,6 +15,6 @@ void LinkedListCreate(Node * * source, int len, int* arr);
 void LinkedListPrint(Node ** head);
 void MergeSort(Node** source); 
 Node* Merge(Node* upper, Node* lower); 
-void SpiltList(Node* source, Node** head1, Node** head2);
+void SplitList(Node* source, Node** head1, Node** head2);
 
 #endif
